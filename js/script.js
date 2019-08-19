05bfa021fff4762b155e72351d9dbdda9c4f1050
unknown <gfalk85@gmail.com>
2019-08-19 14:31:14 +0300

    adding async await promise

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 const cards = document.querySelectorAll('.city-card')
 let cities = [];
 
@@ -5,43 +6,31 @@ for (let i = 0; i < cards.length; i++) {
     cities.push(cards[i].id);
 }
 
-
-
-
-findWeather = (city) => {
-  
-    let cardCity = document.querySelector('#' + city);
+async function fetchWeather(city) {
     var apiPath = 'http://api.openweathermap.org/data/2.5/weather?q=';
     var apiKey = '&APPID=e1e3bc31c8dda22c6d01bb97103eb414&units=metric';
-    var url = apiPath+city+apiKey;  
-    var myRequest = new XMLHttpRequest();
-    myRequest.open('GET', url);
-    myRequest.onload = function() {
-        var getWeather = JSON.parse(myRequest.responseText);
-        var cod = getWeather.cod; 
-       
-       if (cod === 200) {
-        var temp = getWeather.main.temp;
-        var clouds = getWeather.weather[0].main;
+    var url = apiPath+city+apiKey; 
+    let cardCity = document.querySelector('#' + city);
+    const response = await fetch(url);
+    const data = await response.json();
+
+     if (data.cod === 200) {
+        let temp = data.main.temp;
+        let clouds = data.weather[0].main;
         cardCity.childNodes[1].childNodes[0].innerText = "Temeprature in " + city + " now \n"  +  temp + " DG \n " + clouds;
-        
-       }
-        
-        else {
-            
-        } 
-    };
+     
+     }
 
-    myRequest.onerror = function() {
-       console.log('error');
-    };
+    
+}
 
-    myRequest.send();
-};
+
+
+
 
 for (let i = 0; i < cities.length; i++) {
 
-    document.querySelector('#' + cities[i]).addEventListener('mouseover', () => {findWeather(cities[i])});
+    document.querySelector('#' + cities[i]).addEventListener('mouseover', () => {fetchWeather(cities[i])});
 
 }
 
